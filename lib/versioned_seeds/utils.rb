f643829262a67186430da4256cdeb260ec629107
Simon COURTOIS <scourtois@cubyx.fr>
2012-04-10 14:34:49 +0200

    Adding VS::Utils with sql_load

---
+++ lib/versioned_seeds/utils.rb
@@ -0,0 +1,16 @@
+module VersionedSeeds
+  module Utils
+    if defined?(Rails)
+      def self.load_sql(path)
+        sql_file    = VersionedSeeds.root_path + 'db/seeds/' + path
+        db_password = db_config['password']
+
+        system "RAILS_ENV=#{Rails.env} bundle exec rails dbconsole -p #{db_password} < #{sql_file}"
+      end
+
+      def self.db_config
+        @db_config = Rails.application.config.database_configuration[Rails.env]
+      end
+    end
+  end
+end
